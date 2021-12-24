a48a19af300b9f180a08fff62ca2853279d01f0e
Adilet7 <adiletsanjirov942@gmail.com>
2021-12-24 17:45:54 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(number(15, 18));
+    }
+
+    public static String number(int ageOfMe, int temperature) {
+
+        if (ageOfMe > 20 && ageOfMe < 45 && temperature > -20 && temperature < 30) {
+            return "можно идти гулять";
+        } else if (ageOfMe < 20 && temperature > 0 && temperature < 28) {
+            return "  можно идти гулять ";
+        } else if (ageOfMe > 45 && temperature > -10 && temperature < 25) {
+            return "можно идти гулять";
+        }else {
+            return "оставайтесь дома";
+        }
+
+
+    }
+}
+
+
+
+
+
+
